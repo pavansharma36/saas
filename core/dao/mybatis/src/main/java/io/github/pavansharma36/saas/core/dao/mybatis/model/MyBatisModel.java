3e4f76be43fc2a0b660f36822fd6d61543f35482
Pavan Sharma <sharmapavan369@gmail.com>
2025-04-03 00:43:04 +0530

    spotbugs and CI

--- core/dao/mybatis/src/main/java/io/github/pavansharma36/saas/core/dao/mybatis/model/MyBatisModel.java
+++ core/dao/mybatis/src/main/java/io/github/pavansharma36/saas/core/dao/mybatis/model/MyBatisModel.java
@@ -1,5 +1,6 @@
 package io.github.pavansharma36.saas.core.dao.mybatis.model;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.Date;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,8 +9,11 @@ import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 @Getter
 @Setter
 public class MyBatisModel extends BaseMyBatisModel {
+  @SuppressFBWarnings
   protected String id; // NOSONAR for mybatis generator
+  @SuppressFBWarnings
   protected String createdBy; // NOSONAR for mybatis generator
+  @SuppressFBWarnings
   protected Date createdAt; // NOSONAR for mybatis generator
   protected String updatedBy;
   protected Date updatedAt;
--- galaxy/web/src/main/java/io/github/pavansharma36/saas/galaxy/web/servlet/GalaxyDispatcherServlet.java
+++ galaxy/web/src/main/java/io/github/pavansharma36/saas/galaxy/web/servlet/GalaxyDispatcherServlet.java
@@ -1,4 +1,4 @@
-package io.github.pavansharma36.saas.auth.web.servlet;
+package io.github.pavansharma36.saas.galaxy.web.servlet;
 
 import jakarta.servlet.annotation.WebInitParam;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,8 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 @WebServlet(urlPatterns = "/api/*", initParams = {
     @WebInitParam(name = "contextClass", value = "org.springframework.web.context.support.AnnotationConfigWebApplicationContext"),
     @WebInitParam(name = "contextConfigLocation",
-        value = "io.github.pavansharma36.saas.auth.web.config.AuthWebConfiguration")
+        value = "io.github.pavansharma36.saas.galaxy.web.config.GalaxyWebConfiguration")
 }, loadOnStartup = 1, asyncSupported = true)
-public class DispatcherServlet extends org.springframework.web.servlet.DispatcherServlet {
+public class GalaxyDispatcherServlet extends org.springframework.web.servlet.DispatcherServlet {
 
 }
